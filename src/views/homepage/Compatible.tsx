a9b4ec9554fd47623cb8bd85c1df21a88d3aee23
twofaktor <twofaktor@protonmail.com>
2023-11-28 12:36:01 +0100

    Update Compatible.tsx

--- src/views/homepage/Compatible.tsx
+++ src/views/homepage/Compatible.tsx
@@ -78,6 +78,8 @@ export const Compatible = () => {
   useEffect(() => {
     if (inView) {
       setItems([
+        { title: 'v0.17.x-beta', id: 6 },
+        { title: 'v0.16.x-beta', id: 5 },
         { title: 'v0.15.x-beta', id: 4 },
         { title: 'v0.14.x-beta', id: 3 },
         { title: 'v0.13.x-beta', id: 2 },
